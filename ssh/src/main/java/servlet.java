6e99d4341edac18951c7d05649ec177289f7dddc
lixiade <fengfengkun@outlook.com>
2018-09-05 09:50:27 +0800

    仅仅用于测试

---
+++ ssh/src/main/java/servlet.java
@@ -0,0 +1,17 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
+    }
+}
