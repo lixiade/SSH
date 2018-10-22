e3e3c63e8ed1c567b8bb48a52c2120d9f7aee470
kun <40667499+lixiade@users.noreply.github.com>
2018-10-22 17:49:34 +0800

    Add files via upload
    Spring Bean的依赖注入，注解实现，与servlet的整合

---
+++ spring4_1/src/main/java/cap/dao/impl/AdminDAOImpl.java
@@ -0,0 +1,21 @@
+package cap.dao.impl;
+
+import cap.bean.Admin;
+import cap.dao.AdminDAO;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.Resource;
+
+@Repository(value = "adminDAO")
+public class AdminDAOImpl implements AdminDAO {
+    @Resource
+    @Qualifier(value = "admin")
+    private Admin admin;
+
+
+    @Override
+    public String sayHello() {
+        return "您好：" + admin.getUsername();
+    }
+}
---
+++ spring4_1/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<%@page contentType="text/html; utf-8" language="java" pageEncoding="UTF-8" %>
+<html>
+<body>
+<a href="admin">注解的使用</a>
+</body>
+</html>
