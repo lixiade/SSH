aaa1d7195d0b85761a0bfb6fd30da0bd66f4c30e
kun <40667499+lixiade@users.noreply.github.com>
2018-11-02 11:38:21 +0800

    Spring JDBC ,JdbcTemplate核心类的使用

---
+++ spring8x0/src/main/java/cap/dao/AdminDAO.java
@@ -0,0 +1,13 @@
+package cap.dao;
+
+import cap.bean.Admin;
+
+import java.util.List;
+
+public interface AdminDAO {
+    List<Admin> findAdmins();
+    Admin findById(int id);
+    int delAdmin(int id);
+    int addAdmin(Admin admin);
+    int updateAdmin(Admin admin);
+}
